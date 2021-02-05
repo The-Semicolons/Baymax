6c12c71f574a467918373b97204510e8f8540b4d
VanshajGoel <60982919+VanshajGoel@users.noreply.github.com>
2021-02-05 22:03:39 +0530

    Update SpeechAndTextProcessing.py

--- SpeechAndTextProcessing.py
+++ SpeechAndTextProcessing.py
@@ -1,28 +1,33 @@
 #Created by Vanshaj Goel on 29th January 2021
 #pip3 install SpeechRecognition
+#pip3 install pyttsx3
 #File name- SpeechAndTextProcessing.py
-#You just need to make object of class "SpeechTextProcessing" and call the function "TextToSpeech"
+#For Text To Speech--> You just need to make object of class "SpeechTextProcessing" and call the method "SpeechToText"
 #Function will return the detected text, you need to make a variable to store and print the text
-import speech_recognition as sr
+#For Text To Speech--> You need to create object and call the method "TextToSpeech" by passing text as argument
 
+import speech_recognition
+import pyttsx3
 class SpeechTextProcessing:
-    def SpeechToText(self):
-        # Record Audio
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Tell your symptoms!")
-            audio = r.listen(source)
 
-        # Speech recognition using Google Speech Recognition
+    def __init__(self):
+            self.r = speech_recognition.Recognizer()
+            self.engine = pyttsx3.init('sapi5')
+            self.voices = self.engine.getProperty('voices')
+
+    def SpeechToText(self):
+        with speech_recognition.Microphone() as source:
+            audio = self.r.listen(source)
+
         try:
-            # for testing purposes, we're just using the default API key
-            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-            # instead of `r.recognize_google(audio)`
-            RecoginisedText = r.recognize_google(audio)
+            RecoginisedText = self.r.recognize_google(audio)
             return RecoginisedText
-        except sr.UnknownValueError:
-            RecoginisedText="Could not understand audio, please speak clearly"
-            return RecoginisedText
-        except sr.RequestError as e:
-            RecoginisedText="Could not request results! Contact developer of this software {0}".format(e)
-            return RecoginisedText
+        except speech_recognition.UnknownValueError:
+            print("Could not understand audio, please speak clearly")
+        except speech_recognition.RequestError as e:
+            print("Could not request results! Contact developer")
+
+    def TextToSpeech(self, audio):
+        self.engine.setProperty('voice', self.voices[0].id)
+        self.engine.say(audio)
+        self.engine.runAndWait()
